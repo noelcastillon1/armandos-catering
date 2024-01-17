1520887929fa171d27911ea413b6c85705d98f90
noelcastillon1 <noelcastillon1@gmail.com>
2024-01-16 23:41:46 -0500

    style hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,21 @@
 const Hero = () => {
     return (
-        <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)' }}>
+        <div className="hero min-h-screen bg-hero">
             <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+            <div className="hero-content text-center">
+                <div className="">
+                    <h1 className="mb-5 text-5xl font-bold">Let Us Cater
+                        Your Next Event!</h1>
+                    <p className="mb-5">
+                        {`Are you planning a wedding? a quinceañera? how about a graduation party? Nothing brings people together like a quality meal, so let Chef Armando cater your next event! <br />With 5-star service at an unbelievable price, he'll be sure to make your special moments unforgettable!`}
+                    </p>
+                    <div className="hero-btn-wrapper flex justify-center gap-3 flex-col md:flex-row md:justify-around">
+                        <div className="hero-btn-container">
+                            <button className="btn btn-primary">View Our Menus</button></div>
+                        <div className="hero-btn-container">
+                            <button className="btn btn-primary">Make an Appointment</button></div>
+                        <div className="hero-btn-container">
+                            <button className="btn btn-primary">Follow Us</button></div></div>
                 </div>
             </div>
         </div>
