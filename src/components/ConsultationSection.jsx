6bda04227a218ff573aa1c4b8e526c215896472f
noelcastillon1 <noelcastillon1@gmail.com>
2024-03-21 17:07:51 -0400

    add padding and center Consultation section

--- src/components/ConsultationSection.jsx
+++ src/components/ConsultationSection.jsx
@@ -2,10 +2,10 @@ import ConsultForm from "./ConsultForm"
 
 const ConsultationSection = () => {
   return (
-    <>
-        <h4 className="text-5xl py-4">Interested in our services? Let's talk!</h4>
+    <div className="consultation-container px-12">
+        <h4 className="text-5xl py-4 text-center">Interested in our services? Let's talk!</h4>
         <ConsultForm />
-    </>
+    </div>
   )
 }
 
