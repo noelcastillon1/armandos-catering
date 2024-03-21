80a58ebdc578082d89a0175d311ea06c8c4ac944
noelcastillon1 <noelcastillon1@gmail.com>
2024-03-21 16:51:35 -0400

    create ConsultationSection component

---
+++ src/components/ConsultationSection.jsx
@@ -0,0 +1,12 @@
+import ConsultForm from "./ConsultForm"
+
+const ConsultationSection = () => {
+  return (
+    <>
+        <h4 className="text-5xl py-4">Interested in our services? Let's talk!</h4>
+        <ConsultForm />
+    </>
+  )
+}
+
+export default ConsultationSection
