59773cd82d9c1f1cd3f0f2afbc5a8d9b5cebea64
noelcastillon1 <noelcastillon1@gmail.com>
2024-03-21 16:30:42 -0400

    update Reviews component styling

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -10,7 +10,7 @@ const Reviews = () => {
                         </div>
                         <div className="review-text pb-2 px-12">
                             <p className="md:text-2xl">"Armando and his team are absolutely amazing from the food to the customer service he provides. I had my wedding catered from him and his team and everything was super organized and tasted amazing! My wedding was 400 people I was so nervous in finding someone who was able to feed that many people and they handled it with grace! Thank you again I will continue to recommend Armando!"</p>
-                            <p><span className="text-2xl md:text-3xl text-secondary">-Tamara Meraz</span></p>
+                            <p className="text-right"><span className="text-2xl md:text-3xl text-secondary">-Tamara Meraz</span></p>
                         </div>
                     </div>
                     <div id="item2" className="carousel-item w-full grid grid-rows-2 md:grid-rows-none md:grid-cols-2">
@@ -18,7 +18,7 @@ const Reviews = () => {
                             <img className="object-cover w-full h-full" src="./reviewCMT.jpeg" /></div>
                         <div className="review-text pb-2 px-12">
                             <p className="md:text-2xl">"Armando is the greatest chef in the Valley! My family always get him to cook for us in small and Big events! His food is amazing and delicious! Way better than the fancier restaurant here in Fresno! Great service and delicious food what else can we ask for!"</p>
-                            <p><span className="text-xl md:text-3xl text-secondary">-Hassan Macedo</span></p>
+                            <p className="text-right"><span className="text-xl md:text-3xl text-secondary">-Hassan Macedo</span></p>
                         </div>
                     </div>
                     <div id="item3" className="carousel-item w-full grid grid-rows-2 md:grid-rows-none md:grid-cols-2">
@@ -27,7 +27,7 @@ const Reviews = () => {
                         </div>
                         <div className="review-text pb-2 px-12">
                             <p className="md:text-2xl">"High-quality food with excellent flavor, our meal was definitely made with love. Armando’s Catering has great service, will definitely be using his services for all our big or small events! My coworkers along with myself enjoyed our meal so much. 10/10 will be recommending Armando’s catering to our friends and family."</p>
-                            <p><span className="text-xl md:text-3xl text-secondary">-Blake Horwath</span></p>
+                            <p className="text-right"><span className="text-xl md:text-3xl text-secondary">-Blake Horwath</span></p>
                         </div>
                     </div>
                 </div>
