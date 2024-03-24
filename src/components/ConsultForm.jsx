e9ee549df5204cf57fb6d5c95abdac0341c08c8e
noelcastillon1 <noelcastillon1@gmail.com>
2024-03-24 13:36:55 -0400

    add style and responsiveness to form fields

--- src/components/ConsultForm.jsx
+++ src/components/ConsultForm.jsx
@@ -9,6 +9,7 @@ import { CalendarSvg } from "./constants/svgs"
 import { ClockSvg } from "./constants/svgs"
 import { StarSvg } from "./constants/svgs"
 import { NumberSvg } from "./constants/svgs"
+import { QuestionSvg } from "./constants/svgs"
 
 const url = "https://script.google.com/macros/s/AKfycbxI9UC_2019X-UGxMOXIDkIkS8rOSDp6WipD3jQxgbteMUIsHrPWDB6C72isgGYd-ht/exec"
 
@@ -19,10 +20,10 @@ const ConsultForm = () => {
                 fetch(url, {
                     method: 'POST',
                     headers: {
-                      'Content-Type': 'text/plain;charset=utf-8',
+                        'Content-Type': 'text/plain;charset=utf-8',
                     },
                     body: JSON.stringify(values),
-                  })
+                })
                     .then((res) => res.json())
                     .then((values) => console.log('values', values))
                     .catch((err) => console.log('err', err));
@@ -36,17 +37,23 @@ const ConsultForm = () => {
                         submit()
                     }}
                 >
-                    <FormField fieldName={"name"} labelContent={<UserSvg />} placeholder={"Full Name"} changeValidateMessage={"Please enter your full name"} isRequired={true} />
-                    <FormField fieldName={"phoneNum"} labelContent={<PhoneSvg />} placeholder={"Phone Number"} changeValidateMessage={"Please enter your phone number"} isRequired={true} />
-                    <FormField fieldName={"email"} labelContent={<EmailSvg />} placeholder={"Email"} changeValidateMessage={"Please enter your email"} isRequired={true} />
-                    <FormField fieldName={"date"} labelContent={<CalendarSvg />} placeholder={"Event Date"} changeValidateMessage={"Please enter the date of your event"} isRequired={true} />
-                    <FormField fieldName={"time"} labelContent={<ClockSvg />} placeholder={"Event Time"} changeValidateMessage={"Please enter the planned event time"} isRequired={true} />
-                    <FormField fieldName={"type"} labelContent={<StarSvg />} placeholder={"Type of Event"} changeValidateMessage={"Please enter the type of event"} isRequired={true} />
-                    <FormField fieldName={"guestsNum"} labelContent={<NumberSvg />} placeholder={"Approximate Number of Attendees"} changeValidateMessage={"Please enter the planned number of guests"} isRequired={true} />
-                    <FormField fieldName={"comment"} labelContent={""} placeholder={"Full Name"} changeValidateMessage={"Please enter your full name"} isRequired={false} />
-                    <button className="btn btn-primary" disabled={!isValid} type="submit">
-                        Submit
-                    </button>
+                    <div className="form-field-container flex flex-col gap-5 md:mx-6">
+                        <FormField fieldName={"name"} labelContent={<UserSvg />} placeholder={"Full Name*"} changeValidateMessage={"Please enter your full name"} isRequired={true} />
+                        <FormField fieldName={"phoneNum"} labelContent={<PhoneSvg />} placeholder={"Phone Number*"} changeValidateMessage={"Please enter your phone number"} isRequired={true} />
+                        <FormField fieldName={"email"} labelContent={<EmailSvg />} placeholder={"Email*"} changeValidateMessage={"Please enter your email"} isRequired={true} />
+                        <div className="flex flex-col gap-5 md:flex-row">
+                            <FormField fieldName={"date"} labelContent={<CalendarSvg />} placeholder={"Event Date*"} changeValidateMessage={"Please enter the date of your event"} isRequired={true} />
+                            <FormField fieldName={"time"} labelContent={<ClockSvg />} placeholder={"Event Time"} changeValidateMessage={"Please enter the planned event time"} isRequired={false} />
+                        </div>
+                        <FormField fieldName={"type"} labelContent={<StarSvg />} placeholder={"Type of Event*"} changeValidateMessage={"Please enter the type of event"} isRequired={true} />
+                        <FormField fieldName={"guestsNum"} labelContent={<NumberSvg />} placeholder={"Number of Guests*"} changeValidateMessage={"Please enter the planned number of guests"} isRequired={true} />
+                        <FormField fieldName={"comment"} labelContent={<QuestionSvg />} placeholder={"Questions?"} changeValidateMessage={"Please enter your full name"} isRequired={false} />
+                        <div>
+                            <button className="btn btn-primary" disabled={!isValid} type="submit">
+                                Submit
+                            </button>
+                        </div>
+                    </div>
                 </form>
             )}
         </Form>
