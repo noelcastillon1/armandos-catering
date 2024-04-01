68cee4e980586cfb34317eaf010968bd176f5451
noelcastillon1 <noelcastillon1@gmail.com>
2024-04-01 15:49:31 -0400

    change queries to center text on small screens

--- src/components/FoodMenu.jsx
+++ src/components/FoodMenu.jsx
@@ -2,8 +2,8 @@ const FoodMenu = ({ menuName, mainDishes, salads, sides, menuImg, isImgFirst }) 
     return (
         <div className="food-menu-wrapper grid">
             <article className="grid col-span-1 md:grid-cols-2 my-8">
-                <div className="food-menu md:grid md:grid-cols-2 gap-2 px-12">
-                    <h2 className="menu-name col-span-2 w-1/2 py-8 text-6xl lg:text-8xl text-gray-300 z-10">The {menuName} Menu</h2>
+                <div className="food-menu text-center m-auto md:text-left md:m-0 md:px-12 md:grid md:grid-cols-2 gap-2">
+                    <h2 className="menu-name text-center m-auto md:text-left md:m-0 col-span-2 md:w-1/2 py-8 text-6xl lg:text-8xl text-gray-300 z-10">The {menuName} Menu</h2>
                     <div className="main-dishes-container">
                         <p className="main-dishes-subtitle py-4 tracking-widest text-primary uppercase">Main Dishes</p>
                         <ul className="main-dishes-ul leading-loose">
@@ -35,9 +35,9 @@ const FoodMenu = ({ menuName, mainDishes, salads, sides, menuImg, isImgFirst }) 
                                 ))}
                             </ul>
                         </div>
-                    </div>
-                    <div className="menu-button-container py-4 text-center md:text-left">
-                        <a href="/#interestForm"><button className="btn btn-primary">Book This Menu</button></a>
+                        <div className="menu-button-container py-4 text-center md:text-left">
+                            <a href="/#interestForm"><button className="btn btn-primary">Book This Menu</button></a>
+                        </div>
                     </div>
                 </div>
                 <div className={`menu-img-container h-96 md:h-full w-full ${isImgFirst == true ? "md:order-first" : ""}`}>
