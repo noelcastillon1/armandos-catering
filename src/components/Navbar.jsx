da7aa0eb4ac807cff15d1721842114db3a6d45fd
noelcastillon1 <noelcastillon1@gmail.com>
2024-04-05 14:12:12 -0400

    add anchors to navbar dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,9 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                     </div>
                     <ul tabIndex="0" className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Menus</a></li>
-                        <li><a>Make an Appointment</a></li>
-                        <li><a>Follow Us</a></li>
+                        <li><a href="#menus">Menus</a></li>
+                        <li><a href="#interestForm">Make an Appointment</a></li>
+                        <li><a href="#contact">Follow Us</a></li>
                     </ul>
                 </div>
             </div>
