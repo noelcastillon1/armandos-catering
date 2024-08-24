d25a06042c6c23c157d382ed8503164d61737773
noelcastillon1 <noelcastillon1@gmail.com>
2024-08-24 17:30:08 -0400

    add dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,18 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, },
+  env: { browser: true, es2020: true, "jest/globals": true, },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "plugin:jest/recommended",
+    "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "jest"],
   rules: {
     "react/prop-types": "off",
     "react-refresh/only-export-components": [
