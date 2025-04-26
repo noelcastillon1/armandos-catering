89763083eb640a1f2fe0c7353616a3c417eeae50
noelcastillon1 <noelcastillon1@gmail.com>
2025-04-25 23:59:20 -0400

    implement react router, add page, edit navbar

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import ConsultationPage from './pages/ConsultationPage'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/schedule",
+    element: <ConsultationPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
--- src/pages/ConsultationPage.jsx
+++ src/pages/ConsultationPage.jsx
@@ -2,11 +2,11 @@ import ConsultForm from "../components/ConsultForm"
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-const ConsultationSection = () => {
+const ConsultationPage = () => {
   return (
     <>
       <Navbar />
-      <div id="interestForm" className="consultation-container px-12">
+      <div id="interestForm" className="consultation-container px-12 py-4">
         <h4 className="text-5xl py-4 text-center">Interested in our services? Let's talk!</h4>
         <ConsultForm />
       </div>
@@ -15,4 +15,4 @@ const ConsultationSection = () => {
   )
 }
 
-export default ConsultationSection
+export default ConsultationPage
